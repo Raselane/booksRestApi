edf1bb996942cf79fb00fe5d51adb7a963306a18
raselane <refiloe.raselane@bothouniversity.com>
2022-09-08 14:32:22 +0200

    Adding routes

--- routes/books.js
+++ routes/books.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Book = require("../models/books");
-
+// Creat a book
 router.post("/", (req, res) =>{
     const book = new Book({
         name:req.body.bookName,
@@ -19,4 +19,43 @@ router.post("/", (req, res) =>{
     })
 })
 
+// Get all books
+router.get("/", async(req, res)=>{
+   const books = await Book.find();
+   if(!books) res.status(500).send("Something went wrong",error)
+   res.send(books);
+        
+})
+
+//GET Book by Id
+
+router.get("/:BookId", async(req, res)=>{
+   const book = await Book.findById(req.params.BookId);
+        if(!book) res.status(404).send("Book not found");
+        res.send(book);
+})
+
+//UPDATED based on ID
+
+router.put("/:bookId", async(req, res)=>{
+    const updatedBook = await Book.findByIdAndUpdate(req.params.bookId, {
+        name: req.body.bookName,
+        author:{
+            name: req.body.authorName,
+            age: req.body.authorAge,
+        },
+        genre: req.body.genre,
+    }, {new:true});
+
+    if(!updatedBook) res.status(404).send("book not found");
+    res.send(updatedBook);
+})
+
+// Delete a book
+
+router.delete("/:bookId", async(req, res)=>{
+    const deletedBook = await Book.findByIdAndRemove(req.params.bookId);
+    if(!deletedBook) res.status(404).send("book not found");
+    res.send(deletedBook);
+})
 module.exports = router;
